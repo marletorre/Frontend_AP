f16964f55954532ba2573678a8cad91f18f5eb84
Marlene <marlene.torrealbaf@gmail.com>
2022-10-04 15:33:30 -0300

    Se crean los componentes del portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    PortfolioComponent,
+    IniciarSesionComponent
   ],
   imports: [
     BrowserModule,
