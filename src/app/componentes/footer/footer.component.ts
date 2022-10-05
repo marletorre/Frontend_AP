5408fc9f31d190a60a1730306707c433e549d57e
Marlene <marlene.torrealbaf@gmail.com>
2022-10-05 14:24:20 -0300

    se crea codigo footer

--- src/app/componentes/footer/footer.component.ts
+++ src/app/componentes/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub,faLinkedin,faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  faGithub=faGithub;
+  faLinkedin=faLinkedin;
+  faInstagram=faInstagram;
   constructor() { }
 
   ngOnInit(): void {
